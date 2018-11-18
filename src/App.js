c34eabbd21329342d7b74afd1ee3c678bbb47fe3
Uros Trstenjak <uros.trstenjak@gmail.com>
2018-11-18 22:32:33 +0100

    add hot module reloading and render a list of items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// #1: declaration of App as component, which is instantiated with <App />
+// #2: instance of the App component can be used as element in application
+// #3: components are build from elements
 class App extends Component {
   render() {
+    const list = [
+      {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+      },
+      {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+      },
+    ];
+    const user = {
+      first_name: "John",
+      last_name: "Doe"
+    }
+    const title = "My first React app";
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>{title}</h2>
+        <p>Welcome {user["first_name"]} {user["last_name"]}.</p>
+        { list.map(function(item) {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// #1: rendering App as element into a root DOM node
+// #2: everything inside root DOM node is managed by ReactDOM
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+// #1: prevents page refresh & reloads application after change
+// #2: enables keeping application state
+if (module.hot) {
+  module.hot.accept()
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
